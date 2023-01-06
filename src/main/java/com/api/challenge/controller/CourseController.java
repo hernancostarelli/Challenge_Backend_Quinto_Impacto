e296f5ab7d19581654c804d087cabcf66d9c1f25
Hernán Federico Costarelli <hernancostarelli@gmail.com>
2023-01-06 00:31:09 -0300

    feat(feat): Feat the following classes
       CourseController.java
       StudentController.java

---
+++ src/main/java/com/api/challenge/controller/CourseController.java
@@ -0,0 +1,101 @@
+package com.api.challenge.controller;
+
+import com.api.challenge.exception.CourseException;
+import com.api.challenge.exception.StudentException;
+import com.api.challenge.exception.TeacherException;
+import com.api.challenge.model.entity.Course;
+import com.api.challenge.service.ICourseService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/course")
+@RequiredArgsConstructor
+public class CourseController {
+
+    private static final String REDIRECT_TEACHER_SEARCH_TEACHER = "redirect:/course/search-course";
+    private final ICourseService service;
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
+    @GetMapping("/dashboard")
+    public String dashboardCourse() {
+        return "/course/dashboard_course.html";
+    }
+
+    @GetMapping("/search-course")
+    public String searchCourse(ModelMap model, @RequestParam(required = false) String value) throws CourseException {
+        List<Course> courseList;
+        courseList = service.getByValue(value);
+        model.put("courseList", courseList);
+        return "/course/get_course.html";
+    }
+
+    @GetMapping("/save-course")
+    public String saveCourse() {
+        return "/course/save_course.html";
+    }
+
+    @PostMapping("/save-course")
+    public String saveCourse(ModelMap model, @RequestParam String name, @RequestParam String turn,
+                             @RequestParam String schedule, RedirectAttributes redirectAttributes) {
+        try {
+            service.save(name, turn, schedule);
+        } catch (Exception exception) {
+            logger.info("OCURRIÓ UN ERROR : {}", exception.getMessage());
+            model.put("error", exception.getMessage());
+            redirectAttributes.addFlashAttribute("name", name);
+            redirectAttributes.addFlashAttribute("turn", turn);
+            redirectAttributes.addFlashAttribute("schedule", schedule);
+            return "error.html";
+        }
+        return "redirect:/course/dashboard";
+    }
+
+    @GetMapping("/modify-course/{id}")
+    public String modifyCourse(ModelMap model, @PathVariable String id) throws CourseException {
+        model.put("course", service.getById(id));
+        return "/course/modify_course.html";
+    }
+
+    @PostMapping("/modify-course/{id}")
+    public String modifyCourse(@PathVariable String id, @RequestParam String name, @RequestParam String turn,
+                               @RequestParam String schedule, RedirectAttributes redirectAttributes) throws CourseException {
+        service.modify(id, name, turn, schedule);
+        return REDIRECT_TEACHER_SEARCH_TEACHER;
+    }
+
+    @GetMapping("/delete-course/{id}")
+    public String deleteCourse(@PathVariable String id) throws StudentException, TeacherException, CourseException {
+        service.delete(id);
+        return REDIRECT_TEACHER_SEARCH_TEACHER;
+    }
+
+    @GetMapping("/enable-course/{id}")
+    public String enableCourse(@PathVariable String id) throws CourseException {
+        service.enable(id);
+        return REDIRECT_TEACHER_SEARCH_TEACHER;
+    }
+
+    @GetMapping("/disable-course/{id}")
+    public String disableCourse(@PathVariable String id) throws StudentException, CourseException {
+        service.disable(id);
+        return REDIRECT_TEACHER_SEARCH_TEACHER;
+    }
+
+    @GetMapping("/detail-course/{id}")
+    public String detailCourse(ModelMap model, @PathVariable String id) throws CourseException {
+        model.put("course", service.getById(id));
+        return "/course/detail_course.html";
+    }
+}
---
+++ src/main/java/com/api/challenge/controller/StudentController.java
@@ -0,0 +1,139 @@
+package com.api.challenge.controller;
+
+import com.api.challenge.exception.CourseException;
+import com.api.challenge.exception.StudentException;
+import com.api.challenge.model.entity.Course;
+import com.api.challenge.model.entity.Student;
+import com.api.challenge.service.ICourseService;
+import com.api.challenge.service.IStudentService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private static final String REDIRECT_STUDENT_SEARCH_STUDENT = "redirect:/student/search-student";
+    private static final String STUDENT = "student";
+    private final IStudentService service;
+    private final ICourseService courseService;
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+    @GetMapping("/dashboard")
+    public String dashboardStudent() {
+        return "/student/dashboard_student.html";
+    }
+
+    @GetMapping("/search-student")
+    public String searchStudent(ModelMap model, @RequestParam(required = false) String value) throws StudentException {
+        List<Student> studentList;
+        studentList = service.getByValue(value);
+        model.put("studentList", studentList);
+        return "/student/get_student.html";
+    }
+
+    @GetMapping("/save-student")
+    public String saveStudent() {
+        return "/student/save_student.html";
+    }
+
+    @PostMapping("/save-student")
+    public String saveTeacher(ModelMap model, @RequestParam String name, @RequestParam String surname, @RequestParam String dateOfBirth,
+                              @RequestParam String story, RedirectAttributes redirectAttributes) {
+        try {
+            service.save(name, surname, dateOfBirth, story);
+        } catch (Exception exception) {
+            logger.info("OCURRIÓ UN ERROR : {}", exception.getMessage());
+            model.put("error", exception.getMessage());
+            redirectAttributes.addFlashAttribute("name", name);
+            redirectAttributes.addFlashAttribute("surname", surname);
+            redirectAttributes.addFlashAttribute("dateOfBirth", dateOfBirth);
+            redirectAttributes.addFlashAttribute("story", story);
+            return "error.html";
+        }
+        return "redirect:/student/dashboard";
+    }
+
+    @GetMapping("/modify-student/{id}")
+    public String modifyStudent(ModelMap model, @PathVariable String id) throws StudentException {
+        model.put(STUDENT, service.getById(id));
+        return "/student/modify_student.html";
+    }
+
+    @PostMapping("/modify-teacher/{id}")
+    public String modifyTeacher(@PathVariable String id, @RequestParam String name, @RequestParam String surname,
+                                @RequestParam String dateOfBirth, @RequestParam String story) throws StudentException {
+        service.modify(id, name, surname, dateOfBirth, story);
+        return REDIRECT_STUDENT_SEARCH_STUDENT;
+    }
+
+    @GetMapping("/delete-student/{id}")
+    public String deleteStudent(@PathVariable String id) throws StudentException, CourseException {
+        service.delete(id);
+        return REDIRECT_STUDENT_SEARCH_STUDENT;
+    }
+
+    @GetMapping("/enable-student/{id}")
+    public String enableStudent(@PathVariable String id) throws StudentException {
+        service.enable(id);
+        return REDIRECT_STUDENT_SEARCH_STUDENT;
+    }
+
+    @GetMapping("/disable-student/{id}")
+    public String disableStudent(@PathVariable String id) throws CourseException, StudentException {
+        service.disable(id);
+        return REDIRECT_STUDENT_SEARCH_STUDENT;
+    }
+
+    @GetMapping("/detail-student/{id}")
+    public String detailStudent(ModelMap model, @PathVariable String id) throws StudentException {
+        Student student = service.getById(id);
+        model.put(STUDENT, student);
+        List<Course> courseList = student.getCourseList();
+        model.put("courseList", courseList);
+        return "/student/detail_student.html";
+    }
+
+    @GetMapping("/course/{id}")
+    public String addRemoveCourse(ModelMap model, @PathVariable String id) throws CourseException, StudentException {
+        List<Course> courseList = courseService.getForEnable();
+        model.put("course", courseList);
+        Student student = service.getById(id);
+        model.put(STUDENT, student);
+        return "/student/add_remove_course.html";
+    }
+
+    @PostMapping("/add-course/{id}/course/{id2}")
+    public String addCourse(ModelMap model, @PathVariable String id, @PathVariable String id2) throws StudentException, CourseException {
+        try {
+            System.err.println("ID STUDENT : " + id);
+            System.err.println("ID COURSE : " + id2);
+            service.addCourse(id, id2);
+            return REDIRECT_STUDENT_SEARCH_STUDENT;
+        } catch (Exception exception) {
+            System.err.println("OCURRIÓ UN ERROR : " + exception.getMessage());
+            model.put("error", exception.getMessage());
+            return "error.html";
+        }
+    }
+
+    @PostMapping("/remove-course/{id}/course/{id2}")
+    public String removeCourse(@PathVariable String id, @PathVariable String id2) throws StudentException, CourseException {
+        System.err.println("ID STUDENT : " + id);
+        System.err.println("ID COURSE : " + id2);
+        service.removeCourse(id, id2);
+        return REDIRECT_STUDENT_SEARCH_STUDENT;
+    }
+}
